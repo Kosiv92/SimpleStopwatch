25898aaf4c5f40ea55217efe029285cd81b5d0c5
Konstantin Sivkov <kosiv92@gmail.com>
2023-07-24 00:01:02 +0800

    Добавьте файлы проекта.

---
+++ SimpleStopwatch/wwwroot/js/site.js
@@ -0,0 +1,51 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+let timer = document.getElementById('timer');
+let startBtn = document.getElementById('startBtn');
+let pauseBtn = document.getElementById('pauseBtn');
+let resetBtn = document.getElementById('resetBtn');
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let interval;
+
+function updateTime() {
+    seconds++;
+    if (seconds === 60) {
+        minutes++;
+        seconds = 0;
+    }
+    if (minutes === 60) {
+        hours++;
+        minutes = 0;
+    }
+    timer.textContent = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+startBtn.addEventListener('click', () => {
+    interval = setInterval(updateTime, 1000);
+    startBtn.disabled = true;
+    pauseBtn.disabled = false;
+    resetBtn.disabled = false;
+});
+
+pauseBtn.addEventListener('click', () => {
+    clearInterval(interval);
+    startBtn.disabled = false;
+    pauseBtn.disabled = true;
+});
+
+resetBtn.addEventListener('click', () => {
+    clearInterval(interval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    timer.textContent = '00:00:00';
+    startBtn.disabled = false;
+    pauseBtn.disabled = true;
+    resetBtn.disabled = true;
+});
